d66f32b8077c392fc9da9aa601be26a9288a6cc7
crimsonshade <hueblerik@gmail.com>
2023-12-08 10:50:01 +0100

    update: mail file structure

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ def append_age_after_comment(content):
             content = content[:match.end()] + age_insertion + content[match.end():]
         break
             
-    
     return content
-    
-if __name__ == '__main__':
-    # Read the README file as a single string
+
+def main():
+    # Think about another way of doing this, because saving the holer readme is a huge act and a waste of resources
     with open('README.md', 'r') as file:
         readme_contents = file.read()
         
@@ -31,3 +30,6 @@ if __name__ == '__main__':
 
     with open('README.md', 'w') as file:
         file.write(updated_content)
+    
+if __name__ == '__main__':
+    main()
