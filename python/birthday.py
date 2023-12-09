17de4ef88b77c31db0674c2429630f218516daf5
crimsonshade <hueblerik@gmail.com>
2023-12-09 01:48:51 +0100

    modify: make birthday calcultor stand by itself

--- python/birthday.py
+++ python/birthday.py
@@ -1,68 +1,46 @@
 from datetime import datetime
 import re
 
-class Birthday:
-    def __init__(self) -> None:
-        pass
+def get_pattern_matches(content):
+    pattern = re.compile(r'(<!--\s*age:(\d{4})-(\d{1,2})-(\d{1,2})\s*-->)(?:\s*(\d+))?')
+    matches = pattern.finditer(content)
     
-    def get_pattern_matches(self, content):
-        """Tries to find a specific pattern inside the found content
+    return matches
 
-        Args:
-            content (list): All lines read of the imported file
+def get_birthday(match):
+    year, month, day = map(int, (match.group(2), match.group(3), match.group(4)))
+    birthdate = datetime(year, month, day)
+    return birthdate
 
-        Returns:
-            list: A list of all matches, where the pattern fits
-        """
+def calculate_age(birthdate):
+    today = datetime.now()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return age
+
+def main(content):
+    for match in get_pattern_matches(content):
+        birthdate = get_birthday(match)
+        age = calculate_age(birthdate)
         
-        pattern = re.compile(r'(<!--\s*age:(\d{4})-(\d{1,2})-(\d{1,2})\s*-->)(?:\s*(\d+))?')
-        matches = pattern.finditer(content)
-        
-        return matches
-    
-    def get_birthday(self, match):
-        """Extracts my birthdate out of the birthday comment inside the README file.
-
-        Args:
-            match (list): The correct values of my birthday found inside the comment
-
-        Returns:
-            int: My birthdate as a datetime
-        """
-        
-        year, month, day = map(int, (match.group(2), match.group(3), match.group(4)))
-        birthdate = datetime(year, month, day)
-        return birthdate
-
-    def calculate_age(self, birthdate):
-        """Calculates my current age by using the datetime package
-
-        Args:
-            birthdate (datetime): the date of my birth.
-
-        Returns:
-            int: My age
-        """
-        
-        today = datetime.now()
-        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-        return age
-    
-    def append_age_after_comment(self, content):
-        for match in self.get_pattern_matches(content):
-            birthdate = self.get_birthday(match)
-            age = self.calculate_age(birthdate)
+        existing_age = match.group(4)
+        if existing_age:
+            start_index = match.end() - len(existing_age)
+            end_index = match.end()
             
-            existing_age = match.group(4)
-            if existing_age:
-                start_index = match.end() - len(existing_age)
-                end_index = match.end()
-                
-                age_insertion = f"{age}"
-                content = content[:start_index] + age_insertion + content[end_index:]
-            else:
-                age_insertion = f" {age}"
-                content = content[:match.end()] + age_insertion + content[match.end():]
-            break
-            
-        return content
+            age_insertion = f"{age}"
+            content = content[:start_index] + age_insertion + content[end_index:]
+        else:
+            age_insertion = f" {age}"
+            content = content[:match.end()] + age_insertion + content[match.end():]
+        break
+        
+    return content
+
+if __name__ == '__main__':
+    with open('README.md', 'r') as file:
+        readme_contents = file.read()
+    
+    updated_content = main(readme_contents)
+
+    with open('README.md', 'w') as file:
+        file.write(updated_content)
